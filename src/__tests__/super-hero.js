b015a4087656d27a5914678d3d95b819eb71b8ae
Edinson Nuñez More <nmedinson@gmail.com>
2020-04-30 01:51:32 -0500

    add toMatchInlineSnapshot

--- src/__tests__/super-hero.js
+++ src/__tests__/super-hero.js
@@ -1,5 +1,26 @@
 import { getFlyingSuperHeros } from "../super-heros";
-test("", () => {
+test("shoould return fly heors with toMatchSnapshot", () => {
 	const flyHeros = getFlyingSuperHeros();
 	expect(flyHeros).toMatchSnapshot();
+});
+test("shoould return fly heors with toMatchInlineSnapshot", () => {
+	const flyHeros = getFlyingSuperHeros();
+	expect(flyHeros).toMatchInlineSnapshot(`
+		Array [
+		  Object {
+		    "name": "Dynaguy",
+		    "powers": Array [
+		      "disintegration ray",
+		      "fly",
+		    ],
+		  },
+		  Object {
+		    "name": "Apogee",
+		    "powers": Array [
+		      "gravity control",
+		      "fly",
+		    ],
+		  },
+		]
+	`);
 });
